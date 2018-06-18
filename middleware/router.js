124d79220c5d99160be6e9218d00706cea0496bf
Apon Palanuwech <palaa159@newschool.edu>
2018-06-18 10:53:20 +0700

    - update

--- middleware/router.js
+++ middleware/router.js
@@ -1,4 +1,12 @@
-export default function ({ isHMR, app, store, route, params, error, redirect }) {
+export default function ({
+  isHMR,
+  app,
+  store,
+  route,
+  params,
+  error,
+  redirect
+}) {
   // If middleware is called from hot module replacement, ignore it
   if (isHMR) return
 }
