e5ec92a5acd3119bc642b2ecb3b67d35e97aacc2
Apon Palanuwech <palaa159@newschool.edu>
2018-06-25 11:24:08 +0700

    - update

--- pages/index.vue
+++ pages/index.vue
@@ -33,13 +33,43 @@
             class="_h-128px _w-128px _bgrp-nrp _bgpst-ct _bgs-cv"/>
         </div>
       </div>
+      <!-- Slider -->
+      <div class="row">
+        <div class="col-12">
+          <div v-swiper:mySwiper="swiperOption">
+            <div class="swiper-wrapper">
+              <div 
+                v-for="(banner, i) in banners" 
+                :key="i"
+                class="swiper-slide">
+                <!-- <img :src="banner"> -->
+                <div 
+                  v-lazy:background="banner" 
+                  class="_h-512px _bgrp-nrp _bgs-cv _bgpst-ct">xxx</div>
+              </div>
+            </div>
+            <div class="swiper-pagination"/>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data: () => ({
+    banners: [
+      'https://images.unsplash.com/photo-1529835299686-53bd13fb3ee1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=acef595b97346a9bf062c9e8661e8bf1&auto=format&fit=crop&w=1050&q=80',
+      'https://images.unsplash.com/photo-1529772942463-35a62ea332b8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=70760fa9ad67d66ba9ca71825026a794&auto=format&fit=crop&w=1050&q=80',
+      'https://images.unsplash.com/photo-1529786192234-3b32063c9e4b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=015c09878b5a3e1d4f2b7cab57b33b28&auto=format&fit=crop&w=675&q=80'
+    ],
+    swiperOption: {
+      pagination: {
+        el: '.swiper-pagination'
+      }
+    },
+  })
 }
 </script>
 
--- plugins/no-ssr.js
+++ plugins/no-ssr.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import VueFloatLabel from 'vue-float-label'
-import VueCard from 'vue-credit-card'
+// import VueCard from 'vue-credit-card'
 import WebFont from 'webfontloader'
+const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr')
 
 Vue.use(VueFloatLabel)
-Vue.component('Card', VueCard)
+// Vue.component('Card', VueCard)
 
 WebFont.load({
   google: {
     families: ['Kanit']
   }
 })
+
+Vue.use(VueAwesomeSwiper)
